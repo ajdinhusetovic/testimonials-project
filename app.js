0a5f96bd5a6656d34f4e868cd0463bfcf103c74b
ajdin <ajdinhusetovic@outlook.com>
2022-12-04 14:59:30 +0100

    finished the basic css styling and created customer object

---
+++ app.js
@@ -0,0 +1,36 @@
+const review = document.querySelector('#customer-review');
+const img = document.querySelector('img');
+const customerName = document.querySelector('#customer-name');
+
+const customer = [
+    {
+        customerName: 'Ajdin',
+        customerImg: '',
+        customerReview: 'Great site, very interactive!'
+    },
+    {
+        customerName: 'John',
+        customerImg: '',
+        customerReview: "Good people, very helpful can't wait to finalize the buy!"
+    },
+    {
+        customerName: 'Clara',
+        customerImg: '',
+        customerReview: "The workers are very helpful and fast, the only problem I had was a minor one regarding the Android Auto but It's all good now."
+    },
+    {
+        customerName: 'Mile',
+        customerImg: '',
+        customerReview: "Bought my first BMW car today, coulnd't be happier. This is my daily drive :)"
+    },
+    {
+        customerName: 'Jamie',
+        customerImg: '',
+        customerReview: "Poor reliability. My car broke down after 1 year of use and before that it had many problems with the infotainment system. NEVER AGAIN!!"
+    },
+    {
+        customerName: 'Isaac',
+        customerImg: '',
+        customerReview: "Finally bought my dream car and I couldn't be happier about it. It's an BMW X3 2020!!!!"
+    },
+]
